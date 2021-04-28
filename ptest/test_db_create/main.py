c876abe2e8bb4340bd07f5e74e6af235b81f880d
home <chaoquny@cn.ibm.com>
2021-04-28 22:35:51 +0800

    fix test.py

--- ptest/test_db_create/main.py
+++ ptest/test_db_create/main.py
@@ -1,5 +1,6 @@
-from pstraw import Straw
+from pstraw import Straw, Store
 import os
+from datetime import datetime
 
 case_dir = os.path.dirname(os.path.abspath(__file__))
 # work_parh = os.getcwd()
@@ -76,3 +77,48 @@ def CreatePostgresTables():
 
 
 CreatePostgresTables()
+
+'''
+    插入数据
+'''
+@db.sql(SQL_NAME='mysql.ini', SQL_TEMPLATE_TYPE=6)
+def InsertUser(store):
+    return {
+        "NAME": store.NAME,
+        "USERDESC": store.USERDESC,
+        "CREATEBY": 'Chalk Yu',
+        "CREATETIME": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "UPDATEBY":'Chalk Yu',
+        "UPDATETIME": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+@db.sql(SQL_NAME='mysql.ini', SQL_TEMPLATE_TYPE=6)
+def InsertClient(store):
+    return {
+        "USERID": store.USERID,
+        "PHONE": store.PHONE,
+        "CREATEBY": 'Chalk Yu',
+        "CREATETIME": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "UPDATEBY":'Chalk Yu',
+        "UPDATETIME": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+@db.sql(SQL_NAME='mysql.ini', SQL_TEMPLATE_TYPE=3)
+def TruncateTable(table):
+    return {"TBNAME":table}
+
+@db.conn('mysql', ALLOW_ROLLBACK=True)
+def Insert2Mysql():
+    TruncateTable("USER")
+    TruncateTable("CLIENT")
+    for id in range(20):
+        store = Store({
+            "NAME":"Chalk Yu",
+            "USERDESC":f"Chalk Test {str(id+1)}",
+            "USERID":id+1,
+            "PHONE":f'+86-{str(1300000001+id)}'
+        })
+        InsertUser(store)
+        InsertClient(store)
+
+Insert2Mysql()
--- test.py
+++ test.py
@@ -2,27 +2,69 @@ from __future__ import absolute_import
 import sys,os
 # from ptest.test_db_create.main import *
 
+def printHelp():
+    print('''
+ +-----------------------------------
+ |
+ | (1) 批量测试
+ |
+ |    >> python test.py --all
+ |
+ | (2) 初始化数据库
+ |
+ |    >> python test.py --init
+ |
+ | (3) 测试文件
+ |
+ |    >> python test.py test_db_create
+ |    >> python test.py db_create
+ |
+ | (4) 还原数据库后测试
+ |
+ |    >> python test.py --clear test_db_create
+ |    >> python test.py --clear db_create
+ |
+ +-----------------------------------
+''')
+
 if __name__ == '__main__':
 
     if len(sys.argv) > 1:
-        if sys.argv[1] == '--all':
-            dirOmit = [
-                '__pycache__',
-                '__init__.py'
-            ]
-            testCases = list(filter(lambda path:path not in dirOmit,os.listdir('./ptest')))
-            for case in testCases:
-                job = f'from ptest.{case}.main import *'
+        if len(sys.argv) == 2:
+            if sys.argv[1] == '--all' or sys.argv[1] == '-a':
+                dirOmit = [
+                    '__pycache__',
+                    '__init__.py'
+                ]
+                testCases = list(filter(lambda path:path not in dirOmit,os.listdir('./ptest')))
+                for case in testCases:
+                    if case != "test_db_create":
+                        # 还原数据库
+                        from ptest.test_db_create.main import *
+                        # 运行脚本
+                        job = f'from ptest.{case}.main import *'
+                        print('Job-->',job)
+                        exec(job)
+            elif sys.argv[1] == '--help' or sys.argv[1] == '-h' or sys.argv[1] == '-?':
+                printHelp()
+            else:
+                # 运行脚本
+                job = f'from ptest.{sys.argv[1]}.main import *'
                 print('Job-->',job)
                 exec(job)
-        elif sys.argv[1] == '--help' or sys.argv[1] == '-h' or sys.argv[1] == '--h' or sys.argv[1] == '-?' or sys.argv[1] == '--?':
-            print('''
-                # 批量测试
-                python test.py --all
-                # 测试test_db_create
-                python test.py test_db_create
-            ''')
-        else:
+        elif len(sys.argv) == 3 and (sys.argv[1] == '--clear' or sys.argv[1] == '-c'):
+            # 还原数据库
+            from ptest.test_db_create.main import *
+            # 运行脚本
+            job = f'from ptest.{sys.argv[2]}.main import *'
+            print('Job-->',job)
+            exec(job)
+        elif len(sys.argv) == 3 and (sys.argv[2] == '--clear' or sys.argv[2] == '-c'):
+            # 还原数据库
+            from ptest.test_db_create.main import *
+            # 运行脚本
             job = f'from ptest.{sys.argv[1]}.main import *'
             print('Job-->',job)
             exec(job)
+        else:
+            printHelp()
